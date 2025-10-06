b8cb038339721623aea995f787cbaf8ad059d7b6
Maxime <maxime05072008@gmail.com>
2025-10-06 20:11:06 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 import csv
 
-fichier = open('C:/Users/MAX/Documents/Github/Projet vente de jeux/steam-200k.csv','r',encoding=('UTF-8'))
+fichier = open('steam-200k.csv','r',encoding=('UTF-8'))
 sales=list(csv.DictReader(fichier))
+
 print(sales[0])
